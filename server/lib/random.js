b14089f5d248ed945b42f359a6ec9335753266a7
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2025-12-13 07:35:13 -0700

    Enhance bot name selection with random capitalization
    Updated the bot name list and modified chooseBotName to apply random capitalization styles, including all lowercase, all uppercase, title case, and random per-character casing, to increase name variety and realism.

--- server/lib/random.js
+++ server/lib/random.js
@@ -92,12 +92,51 @@ exports.chooseChance = (...arg) => {
 }
 
 exports.nameLists = {
-    bots: ["Bob", "DogeisCut", "Starmatrix_20", "Waffz_The_Pancake", ":3", "Gogux", "", "this game sucks", "gomeux", "diep", "arras", "scenexe", "Triangle", "This is the tale of...", "lol", "lmao", ":p", "❤❤❤", "how do i play", "SKIBIDI TOILET", ":D", "trapper main", "Vadisnotu", "Gravityuse", "Unknown", "team?", "spin to team", "D:", "OwO", "UwU", "furry", "play furgals", "pls spawn radiant", "qwerty", "killer", "tgiwjhrsegbnjekrbnghjkrdhnb", "9 + 10 = ?", "TANK ONLY", "King", "Protogen Lover", "BSOD", "lag", "im new", "TimmyGamer2006", "spin 2 team", "Bot", "(Bot)", "No im real!!", "GREEN TEAM ON TOP!!", "PURPLE TEAM FTW", "bad game", "among gus", "Kris", "moss", "WetSock", "Zlump", "aimgod777", "SirLaggington", "𝒢𝒽𝑜𝓈𝓉", "『𝕍𝕠𝕚𝕕』", "︻╦̵̵͇̿̿̿̿╤──", "dont shoot me", "why me ;-;", "why am i red", "gg", "bruh moment", "wait pls", "what is happening", "ok sure buddy", "bad team", "DONT", "Very Glooby", "IS YOU", "bloons", "i eat sand", "FRICK", "???", "Um??", "3754689347"],
+    bots: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Bob", "i suck at this :(", "swear words", "DogeisCut", "Starmatrix_20", "Waffz_The_Pancake", ":3", "Gogux", "this game sucks", "gomeux", "diep", "arras", "scenexe", "Triangle", "This is the tale of...",
+        "lol", "lmao", ":p", "❤❤❤", "zzz", "press W", "not a bot", "o_o", "tank.exe", "just farming", "STOP SPAWN CAMPING", "💥", "💣", "😊", "tank-chan", "OWO whats this?", "0000000", "i am dogeiscut's best", "snowman", "ねこです", "小心",
+        "how do i play", "SKIBIDI TOILET", ":D", "trapper main", "Vadisnotu", "Gravityuse", "Unknown", "team?", "spin to team", "D:", "OwO", "UwU", "furry", "play furgals", "pls spawn radiant", "qwerty", "killer", "tgiwjhrsegbnjekrbnghjkrdhnb",
+        "9 + 10 = ?", "TANK ONLY", "King", "Protogen Lover", "BSOD", "lag", "im new", "TimmyGamer2006", "spin 2 team", "Bot", "(Bot)", "No im real!!", "GREEN TEAM ON TOP!!", "PURPLE TEAM FTW", "bad game", "among gus", "Kris", "moss", "WetSock",
+        "Zlump", "aimgod777", "SirLaggington", "𝒢𝒽𝑜𝓈𝓉", "『𝕍𝕠𝕚𝕕』", "︻╦̵̵͇̿̿̿̿╤──", "dont shoot me", "why me ;-;", "why am i red", "gg", "bruh moment", "wait pls", "what is happening", "ok sure buddy", "bad team", "DONT", "Very Glooby", "IS YOU", "bloons",
+        "i eat sand", "FRICK", "???", "Um??", "3754689347", "꧁༒𝓓𝓮𝓪𝓭༒꧂", "⚡ 𝓝𝓮𝓸𝓷 ⚡", "👑 𝓟𝓻𝓸 👑", "frog eater", "how do i shoot", "balanced game", "pentagons ruined gomeux", "drip404", "No0bSlayer99", "xX_ProGamerGod_Xx", "StaticWolf09",
+        "tiny", "brain damage", "oof", "im lagging", "Alt + F4", "ctrl + w", "please nerf", "rip server", "Lil Poopdrop™", "我饿了", "delta", "o", "q", "x", "z", "riptide", "🅽🅾 🅽🅰🅼🅴", "【Ｒｅｘ】", "b̾a̾z̾", "foo", "bar", "samantha", "(;", "1273", "256",
+        "[REDACTED]", "___________", "Archimedes", "Ribs grow back!", "oooooughhhhh", "?", "haha", "Huh?", "L", "trying", "i will get you", "✨", "✨ω✨", "fiesta!", "sussy", "oh god??", "long time no c", "K.O.", "bart", "HRTZ", "Defender", "AC"],
     a: ["Archimedes", "Akilina", "Anastasios", "Athena", "Alkaios", "Amyntas", "Aniketos", "Artemis", "Anaxagoras", "Apollon"],
     castle: ["Berezhany", "Lutsk", "Dobromyl", "Akkerman", "Palanok", "Zolochiv", "Palanok", "Mangup", "Olseko", "Brody", "Isiaslav", "Kaffa", "Bilhorod"],
     legion: ["Vesta", "Juno", "Orcus", "Janus", "Minerva", "Ceres"]
 }
 
-exports.chooseBotName = () => exports.choose(exports.nameLists.bots)
+exports.chooseBotName = () => {
+	const chosenBotName = exports.choose(exports.nameLists.bots);
+	const randomCapitalizationRoll = Math.floor(Math.random() * 1000);
+
+	if (randomCapitalizationRoll < 20) {
+		return chosenBotName.split("").map(character => {
+			if (Math.random() < 0.5) {
+				return character.toUpperCase();
+			}
+			return character.toLowerCase();
+		}).join("");
+	}
+
+	if (randomCapitalizationRoll < 170) {
+		return chosenBotName.toLowerCase();
+	}
+
+	if (randomCapitalizationRoll < 320) {
+		return chosenBotName.toUpperCase();
+	}
+
+	if (randomCapitalizationRoll < 470) {
+		return chosenBotName.split(" ").map(word => {
+			if (word.length === 0) {
+				return word;
+			}
+			return word[0].toUpperCase() + word.slice(1).toLowerCase();
+		}).join(" ");
+	}
+
+	return chosenBotName;
+};
 
 exports.chooseBossName = (code, amount) => code in exports.nameLists ? exports.chooseN(exports.nameLists[code], amount) : []
